0bb63da6d4ab09d6af9f78e26e52292fa78f1d46
alanhoudus <alan.houdus@outlook.fr>
2022-02-20 17:12:30 +0100

    added store

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducer from 'src/reducers/reducer';
+import logMiddleware from '../middlewares/logMiddleware';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancers = composeEnhancers(
+  applyMiddleware(
+    logMiddleware,
+
+  ),
+);
+const store = createStore(reducer, enhancers);
+
+export default store;
