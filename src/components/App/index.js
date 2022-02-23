29e7d0c97a8912fae8e2037fb9baf9c8b9b1c213
alanhoudus <alan.houdus@outlook.fr>
2022-02-23 21:22:50 +0100

    basic integration of Messages component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,15 @@
 // == Import
+// hooks
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+// react-router-dom
+import { Route, Routes } from 'react-router-dom';
+// actions
 import { getUserConversations } from '../../actions/user';
+// components
 import Conversations from '../conversationsList';
+import Messages from '../messages';
+// scss
 import './styles.scss';
 
 // == Composant
@@ -15,7 +22,10 @@ const App = () => {
 
   return (
     <div className="app">
-      <Conversations />
+      <Routes>
+        <Route path="/" key="conversations" element={<Conversations />} />
+        <Route path="/messages" key="messages" element={<Messages />} />
+      </Routes>
     </div>
   );
 };
--- src/components/messages/index.js
+++ src/components/messages/index.js
@@ -2,7 +2,26 @@ import './messages.scss';
 
 const Messages = () => (
   <div className="messages">
-    Liste des messages
+    <div className="messages-sender">
+      <div className="messages-sender--name">Jean</div>
+      <div className="messages-sender--message">Premier message de la conversation</div>
+    </div>
+    <div className="messages-recipient">
+      <div className="messages-recipient--name">Pierre</div>
+      <div className="messages-recipient--message">Second message de la conversation</div>
+    </div>
+    <div className="messages-recipient">
+      <div className="messages-recipient--name">Pierre</div>
+      <div className="messages-recipient--message">Second message de la conversation</div>
+    </div>
+    <div className="messages-sender">
+      <div className="messages-sender--name">Jean</div>
+      <div className="messages-sender--message">Premier message de la conversation</div>
+    </div>
+    <div className="messages-sender">
+      <div className="messages-sender--name">Jean</div>
+      <div className="messages-sender--message">Premier message de la conversation</div>
+    </div>
   </div>
 );
 
